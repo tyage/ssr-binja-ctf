223e59b08ccb01db5017b809f99f9162dd0e6c35
tyage <namatyage@gmail.com>
2017-05-14 19:34:50 +0900

    create container

--- containers/App.js
+++ containers/App.js
@@ -1,47 +1,30 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import Counter from '../components/Counter'
 
-class Counter extends Component {
-  static propTypes = {
-    value: PropTypes.number.isRequired,
-    onIncrement: PropTypes.func.isRequired,
-    onDecrement: PropTypes.func.isRequired
-  }
-
-  incrementIfOdd = () => {
-    if (this.props.value % 2 !== 0) {
-      this.props.onIncrement()
-    }
-  }
-
-  incrementAsync = () => {
-    setTimeout(this.props.onIncrement, 1000)
-  }
-
-  render() {
-    const { value, onIncrement, onDecrement } = this.props
-    return (
-      <p>
-        Clicked: {value} times
-        {' '}
-        <button onClick={onIncrement}>
-          +
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          -
-        </button>
-        {' '}
-        <button onClick={this.incrementIfOdd}>
-          Increment if odd
-        </button>
-        {' '}
-        <button onClick={this.incrementAsync}>
-          Increment async
-        </button>
-      </p>
-    )
+const mapStateToProps = (state) => {
+  return {
+    value: state.counter
   }
 }
 
-export default Counter
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrement: () => {
+      return {
+        type: 'INCREMENT'
+      }
+    },
+    onDecrement: () => {
+      return {
+        type: 'INCREMENT'
+      }
+    }
+  }
+}
+
+const App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
+
+export default App
