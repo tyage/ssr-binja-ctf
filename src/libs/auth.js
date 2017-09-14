a43124ed98bc9db13d047f0903dc051fa067abf1
tyage <namatyage@gmail.com>
2017-09-15 00:15:43 +0900

    clear cookies

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -3,5 +3,5 @@ export const isAuthenticated = (cookie) => cookie.get('username') !== undefined
 export const login = (cookie, username) => cookie.set('username', username)
 
 export const logout = (cookie) => {
-  [ 'username', 'idols' ].forEach((key) => cookie.remove(key))
+  Object.keys(cookie.getAll()).forEach((key) => cookie.remove(key))
 }
