0a2bdbd579cf351297b666330c2d4b1dfc98953b
tyage <namatyage@gmail.com>
2017-09-15 14:42:37 +0900

    idolname and back link

--- src/components/Idol.js
+++ src/components/Idol.js
@@ -24,13 +24,18 @@ class Idol extends Component {
 
     return (
       <div>
-        <img src={ idol.image } alt=""/>
-        <p>{ idolAction() }</p>
-        <ul>
-          <li><Link to={ `/idols/${id}/say1` }>voice 1</Link></li>
-          <li><Link to={ `/idols/${id}/say2` }>voice 2</Link></li>
-          <li><Link to={ `/idols/${id}/say3` }>voice 3</Link></li>
-        </ul>
+        <Link to="/idols">Back to my idols.</Link>
+
+        <div>
+          <h2>{ idol.name }</h2>
+          <img src={ idol.image } alt="" />
+          <p>{ idolAction() }</p>
+          <ul>
+            <li><Link to={ `/idols/${id}/say1` }>voice 1</Link></li>
+            <li><Link to={ `/idols/${id}/say2` }>voice 2</Link></li>
+            <li><Link to={ `/idols/${id}/say3` }>voice 3</Link></li>
+          </ul>
+        </div>
       </div>
     )
   }
