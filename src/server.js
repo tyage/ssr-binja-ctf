29d7a2ed5d43bef08825c5a0c3b7f9a9773f1331
tyage <namatyage@gmail.com>
2017-11-03 00:16:20 +0900

    set favicon.ico (fix #1)

--- src/server.js
+++ src/server.js
@@ -37,6 +37,7 @@ function renderPage(appHtml) {
     <meta charset=utf-8/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>SSR</title>
+    <link rel="shortcut icon" href="/public/favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel=stylesheet href=/public/app.css>
 
